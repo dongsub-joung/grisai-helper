bbd7df144d9f0db897c5bf6e1289535debca2a36
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2026-04-30 06:18:07 +0900

    Add) fn save_translated_data

--- src/struct_string/mod.rs
+++ src/struct_string/mod.rs
@@ -12,8 +12,12 @@ struct StringData{
 
 impl StringData{
     pub fn new(string_data: String, contury_code: CONTURIES) -> Self{
-        self{ string_data, contury_code, translated_data: String::new() }
+        Self{ string_data, contury_code, translated_data: String::new() }
     }
 
-
+    pub fn save_translated_data(&mut self, translated_data: String){
+       self.translated_data= translated_data; 
+    }
 }
+
+
