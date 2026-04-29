d22b6c8bba08c0b9717b9a084085944086bed5c1
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2026-04-30 05:55:05 +0900

    Add) fn new() in impl StringData

--- src/struct_string/mod.rs
+++ src/struct_string/mod.rs
@@ -1,4 +1,19 @@
+enum CONTURIES{
+    ENGLISH,
+    RUSSIAN,
+    CHINESE,
+}
+
 struct StringData{
    string_data: String,
+   contury_code: CONTURIES,
    translated_data: String,
+}
+
+impl StringData{
+    pub fn new(string_data: String, contury_code: CONTURIES) -> Self{
+        self{ string_data, contury_code, translated_data: String::new() }
+    }
+
+
 }
