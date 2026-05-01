0ec48ad3c71c059581b58bcd2296884aaa7823bf
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2026-05-01 09:38:21 +0900

    Add) communicate_with_translation_api()

--- src/translation/mod.rs
+++ src/translation/mod.rs
@@ -4,8 +4,26 @@ struct Translation{
     sentence_data: self::struct_string::StringData,
 }
 
+
+#[derive(Debug)]
+struct ApiCallErr;
+
 impl Translation{
-    fn new(sentence_data: self::struct_string::StringData) -> Self {
+    unsafe fn new(sentence_data: self::struct_string::StringData) -> Self {
         Self { sentence_data }
+    }
+
+    // @TODO tokio
+    async fn commuicate_with_translation_api(&self){ // -> self::struct_string::StringData
+        // make .evn
+        const API_KEY: &'static str= "";
+        
+        // request formating
+     
+        // init api request
+        
+        // json dezerialization?
+        
+        // save sentence_data
     }
 }
